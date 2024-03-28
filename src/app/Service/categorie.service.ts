30b828f51a497c426cf3f3007bd6a835262a3d9d
mombouli1998 <trinitemombouli@.com>
2024-03-28 17:11:39 +0100

    à jour

--- src/app/Service/categorie.service.ts
+++ src/app/Service/categorie.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { categorie } from '../model/categorie';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategorieService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  //Recuperer la liste des produits
+  public ListeCategorie():Observable<Array<categorie>>{
+    return this.http.get<Array<categorie>>("http://localhost:3000/categorie");
+}
+
 }
