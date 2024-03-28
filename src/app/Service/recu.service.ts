6fc7d48ea9b24ae9a760ef05bd1c7642e2ad338e
mombouli1998 <trinitemombouli@.com>
2024-03-28 22:36:38 +0100

    mise à jour 16

--- src/app/Service/recu.service.ts
+++ src/app/Service/recu.service.ts
@@ -8,9 +8,107 @@ import autoTable from 'jspdf-autotable';
   providedIn: 'root'
 })
 export class RecuService {
-Recut(panie: Panier) {
-  throw new Error('Method not implemented.');
-}
+
 constructor(){}
+
+public Recut(panier: Panier) {
+  const doc = new jsPDF();
+  let jour:number=new Date().getDate();
+  let moi:number=new Date().getDay();
+  let annee:number=new Date().getFullYear();
+  const rows: any[][] = [];
+  panier.produits.forEach((produit: Produit) => {
+    rows.push([produit.nom, produit.categorie,produit.prix]);
+  });
+
+  let url="https://i.goopics.net/ahazzy.jpg";
+  var x = 10 ; //Position horizontale
+  var y = 10 ; //Position verticale
+  var largeur = 100 ; //Largeur de l’image
+  var hauteur = 100 ; //Hauteur de l’image
+
+
+  doc.addImage(url, 'JPEG', 10, 10, 190, 100); // Ajustez les dimensions et la position selon vos besoins
+  autoTable(doc, {
+    body: [
+      [
+        {
+          content:"Entreprise",
+          styles:{
+            halign:"left",
+            fontSize:20,
+            textColor:"white",
+            
+          }
+        },
+        {
+          content:"M_Trinite",
+          styles:{
+            halign:"right",
+            fontSize:20,
+            textColor:"white"
+          }
+        },
+      ]
+    ],
+    theme:"plain",
+    styles:{
+      fillColor:"blue"
+    }
+  });
+
+  autoTable(doc, {
+    body: [
+      [
+        {
+          content:"Reference : #5485248"+
+          "\n"+jour+"-"+moi+"-"+annee,
+          styles:{
+            halign:"right",
+            fontSize:10,
+            textColor:"Black",
+            
+          }
+        }
+      
+    ],
+  ], 
+  theme:"plain",
+  styles:{
+  fillColor:"white"
+  }
+});
+  
+
+  autoTable(doc, { html: '#my-table' })
+  autoTable(doc, {
+    head: [['Nom',  'Categorie','Prix-unitaire']],
+    body: rows,
+  });
+
+  autoTable(doc, {
+    body: [
+      [
+        {
+          content:"Totale: "+panier.total+" $"+
+           "\n"+jour+"-"+moi+"-"+annee,
+         
+          styles:{
+            halign:"right",
+            fontSize:10,
+            textColor:"Black",
+            
+          }
+        }
+      
+      ],
+    ],
+    theme:"plain",
+    styles:{
+    fillColor:"white"}
+  });
+
+  doc.save('Reçu.pdf');
+}
 
 }
