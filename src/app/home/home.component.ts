ebe53aefab1e141f0b6fcad1a57bc6ae17683435
mombouli1998 <trinitemombouli@.com>
2024-03-28 14:31:35 +0100

    mis à jour 10

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit';
+import { ProduitService } from '../Service/produit.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  ListeProduit:Array<Produit>=[];
+  recherche!:string;
+    constructor(private ps:ProduitService, private route:Router){}
+    ngOnInit(): void {
+     this.ps.ListeProduits().subscribe({
+      next:data=>{
+        this.ListeProduit=data;
+      },
+      error(err) {
+          console.log(err);
+      },
+     })
+    }
+    //Recherche produit
+    Search() {
+      this.ps.Recherche(this.recherche).subscribe({
+        next:data=>{this.ListeProduit=data},
+        error(err) {
+            console.log(err);
+        },
+      })   
+    }
+    //acheter produit
+    acheter(produit: Produit) {
+      this.route.navigateByUrl("/acheter/"+produit.id);
+      }
 }
