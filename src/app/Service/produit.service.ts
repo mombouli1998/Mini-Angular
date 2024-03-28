f67480723b6c0c51a9061b7ffbd0506b65530aa3
mombouli1998 <trinitemombouli@.com>
2024-03-28 16:55:46 +0100

    base de donnees produit

--- src/app/Service/produit.service.ts
+++ src/app/Service/produit.service.ts
@@ -13,11 +13,11 @@ export class ProduitService {
   //Recuperer la liste des produits
   public ListeProduitsPage(page:number=1,size:number=5):Observable<Array<Produit>>{
     //let size:number=5;
-    return this.http.get<Array<Produit>>(`http://localhost:3000/produits?_start=${page}&_limit=${size}`);
+    return this.http.get<Array<Produit>>(`https://banque-7bq4.onrender.com?_start=${page}&_limit=${size}`);
 }
   //Recuperer la liste des produits
   public ListeProduits():Observable<Array<Produit>>{
-      return this.http.get<Array<Produit>>("http://localhost:3000/produits");
+      return this.http.get<Array<Produit>>("https://banque-7bq4.onrender.com/produits");
   }
   //modifier le produit cocher
   public cocher(produit:Produit):Observable<Produit>{
@@ -25,32 +25,32 @@ export class ProduitService {
   }
   //supprimer le produit
   public DeleteProduit(produit:Produit){
-    return this.http.delete<Produit>("http://localhost:3000/produits/"+produit.id,);
+    return this.http.delete<Produit>("https://banque-7bq4.onrender.com/produits/"+produit.id,);
   }
   //ajouter un produit
   public AjoutProdui(produit:Produit){
-    return this.http.post<Produit>("http://localhost:3000/produits",produit);
+    return this.http.post<Produit>("https://banque-7bq4.onrender.com/produits",produit);
   }
   //recherche de produit
   public Recherche(valeur:string):Observable<Array<Produit>>{
-    return this.http.get<Array<Produit>>("http://localhost:3000/produits?nom="+valeur);
+    return this.http.get<Array<Produit>>("https://banque-7bq4.onrender.com/produits?nom="+valeur);
   }
   //retourn le produit id
   public getProduit(id:any):Observable<Produit>{
-    return this.http.get<Produit>("http://localhost:3000/produits/"+id);
+    return this.http.get<Produit>("https://banque-7bq4.onrender.com/produits/"+id);
   }
   //modifie le produit
   public Modifie(produit:Produit):Observable<Produit>{
-    return this.http.put<Produit>("http://localhost:3000/produits/"+produit.id,produit);
+    return this.http.put<Produit>("https://banque-7bq4.onrender.com/produits/"+produit.id,produit);
   }
   //liste des produits categorie 
   public listCategorie(valeur:string):Observable<Array<Produit>>{
     
-    return this.http.get<Array<Produit>>("http://localhost:3000/produits?categorie="+valeur);
+    return this.http.get<Array<Produit>>("https://banque-7bq4.onrender.com/produits?categorie="+valeur);
   }
   //recherche de produit par rapport à leur categorie
   public RechercheProduit(valeur:string,objet:string):Observable<Array<Produit>>{
-    return this.http.get<Array<Produit>>(`http://localhost:3000/produits?nom=${objet}&categorie=${valeur}`);
+    return this.http.get<Array<Produit>>(`https://banque-7bq4.onrender.com/produits?nom=${objet}&categorie=${valeur}`);
   }
 
 }
