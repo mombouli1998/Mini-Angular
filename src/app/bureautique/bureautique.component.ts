5037bb2493d12c97761568aba64446db494889ab
mombouli1998 <trinitemombouli@.com>
2024-03-28 14:55:00 +0100

    mis à jour 10

--- src/app/bureautique/bureautique.component.ts
+++ src/app/bureautique/bureautique.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit';
+import { ProduitService } from '../Service/produit.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bureautique',
   templateUrl: './bureautique.component.html',
   styleUrls: ['./bureautique.component.css']
 })
-export class BureautiqueComponent {
+export class BureautiqueComponent implements OnInit {
+  recherche!:string;
+  ListeProduit!:Array<Produit>;
+  constructor(private Ps:ProduitService, private route:Router){}
+  ngOnInit(): void {
+   this.Ps.listCategorie("bureautique").subscribe({
+    next:data=>{
+      this.ListeProduit=data;
+    },
+    error(err) {
+        console.log(err);
+    },
+   })
+  }
+  //recherche des produits de bureautiques
+  Search() {
+   this.Ps.RechercheProduit("bureautique",this.recherche).subscribe({
+    next:data=>{this.ListeProduit=data},
+    error(err) {
+        console.log(err);
+    },
+   })
+    }
+    //acheter le produit
+    acheter(produit: Produit) {
+      this.route.navigateByUrl("/acheter/"+produit.id);
+      }
 
 }
