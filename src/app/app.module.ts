cf74d0bb188738f8a6e668f289675a218cc45274
mombouli1998 <trinitemombouli@.com>
2024-03-28 02:37:18 +0100

    mis à jour 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { ProduitComponent } from './produit/produit.component';
+import { BureautiqueComponent } from './bureautique/bureautique.component';
+import { ConsoleComponent } from './console/console.component';
+import { JeuComponent } from './jeu/jeu.component';
+import { TelephoneComponent } from './telephone/telephone.component';
+import { EditeProduitComponent } from './edite-produit/edite-produit.component';
+import { NewProduitComponent } from './new-produit/new-produit.component';
+import { CommandeComponent } from './commande/commande.component';
+import { PanierComponent } from './panier/panier.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProduitComponent,
+    BureautiqueComponent,
+    ConsoleComponent,
+    JeuComponent,
+    TelephoneComponent,
+    EditeProduitComponent,
+    NewProduitComponent,
+    CommandeComponent,
+    PanierComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
