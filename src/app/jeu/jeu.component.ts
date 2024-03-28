fc82a15859267da42d7c21917c8d8e329aaf6a0f
mombouli1998 <trinitemombouli@.com>
2024-03-28 14:58:30 +0100

    mis à jour 10

--- src/app/jeu/jeu.component.ts
+++ src/app/jeu/jeu.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit';
+import { ProduitService } from '../Service/produit.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-jeu',
   templateUrl: './jeu.component.html',
   styleUrls: ['./jeu.component.css']
 })
-export class JeuComponent {
+export class JeuComponent implements OnInit{
+  recherche!:string;
+  ListeProduit!:Array<Produit>;
+  constructor(private Ps:ProduitService, private route:Router){}
+  ngOnInit(): void {
+   this.Ps.listCategorie("jeux-video").subscribe({
+    next:data=>{
+      this.ListeProduit=data;
+    },
+    error(err) {
+        console.log(err);
+    },
+   })
+  }
+  //recherche des produits de bureautiques
+  Search() {
+    alert(this.recherche)
+   this.Ps.RechercheProduit("jeux-video",this.recherche).subscribe({
+    next:data=>{this.ListeProduit=data},
+    error(err) {
+        console.log(err);
+    },
+   })
+    }
+    //acheter le produit
+    acheter(produit: Produit) {
+      this.route.navigateByUrl("/acheter/"+produit.id);
+      }
 
 }
